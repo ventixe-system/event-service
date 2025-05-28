27e97bca09f7baed3c2f7e93c10804d37670c587
Maria Falk <mariacelf@gmail.com>
2025-05-28 14:33:16 +0200

    Added entities for events and ticketpackages.

---
+++ EventService.Api/Entities/EventEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace EventService.Api.Entities;
+
+public class EventEntity
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } =null!;
+    public string? ImageUrl { get; set; } = null!;
+    public int CategoryId { get; set; }
+    public string Status { get; set; } = null!;
+    public DateTime Date {  get; set; }
+    public DateTime StartTime { get; set; }
+    public string? Location { get; set; }
+    public decimal StartingPrice { get; set; }
+
+    public ICollection<TicketPackagesEntity> TicketPackages { get; set; } = [];
+}
---
+++ EventService.Api/Entities/TicketPackagesEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace EventService.Api.Entities;
+
+public class TicketPackagesEntity
+{
+    public int Id { get; set; }
+    public Guid EventId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int TotalQuantity { get; set; }
+    public int SoldQuantity { get; set; }
+
+    public EventEntity Event { get; set; } = null!;
+}
