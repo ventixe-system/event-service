01f929d3e3eef9e07ce0f6bae70384024c23a474
Maria Falk <mariacelf@gmail.com>
2025-05-28 18:56:30 +0200

    Installed EntityFrameworkCore and SQLServed dependencies, started writing EventDbContext

---
+++ EventService.Api/Data/Context/EventDbContext.cs
@@ -0,0 +1,10 @@
+﻿using EventService.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventService.Api.Data.Context;
+
+public class EventDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<EventEntity> Events { get; set; }
+    public DbSet<TicketPackagesEntity> TicketPackages { get; set; }
+}
